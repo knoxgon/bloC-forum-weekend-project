a4db835626c82a4b2e941fb97af8cc3d77b48d39
knoxgon <volkan.guven@hotmail.se>
2019-07-03 19:42:20 +0200

    First initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SalesComponent } from './sales/sales.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
