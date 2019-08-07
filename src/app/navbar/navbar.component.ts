f37ae9e98565a046738ba28fcfa69c92f0d3c0c4
Ismail Güven <ismailguven4200@gmail.com>
2019-08-07 12:13:03 +0200

    Added login popup

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    const loginContainer = document.getElementById('login-modal-container');
+    const closeButton = document.getElementsByClassName('login-cancel-button')[0];
+    const loginButton = document.getElementById('loginBtn');
+
+    window.onclick = (event) => {
+      if (event.target === loginContainer || event.target === closeButton)
+        loginContainer.style.display = 'none';
+      else if (event.target === loginButton )
+        loginContainer.style.display = 'block';
+    };
+  }
 }
